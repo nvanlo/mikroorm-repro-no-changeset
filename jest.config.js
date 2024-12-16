c8c41085bfafa13c2f0efdea09261e3baff5051d
nvanlo <nik.vanlooy@nike.com>
2024-12-16 15:18:18 +0100

    Small change

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     testTimeout: 30000,
+    testEnvironment: "node",
+    roots: ["<rootDir>/src"],
+    testMatch: ["**/*test.ts"],
     transform: {
-        '^.+\\.tsx?$': ['ts-jest', {
-            // tsconfig: 'tsconfig.json',
-            isolatedModules: true,
-        }],
+        "^.+\\.tsx?$": "ts-jest",
     },
 };
